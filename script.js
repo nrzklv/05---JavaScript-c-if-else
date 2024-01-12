3a4d76575d72aa4afea4a93f65b7612888bfe672
nsx_69 <mrmanafest16@gmail.com>
2024-01-12 20:14:53 +0500

    Self Study task

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const price = 100;
-const discount = 20;
-const total = price - (price * discount / 100);
+// const price = 100;
+// const discount = 20;
+// const total = price - (price * discount / 100);
+//
+// if (total >= 90) {
+//     alert("Не берём!")
+// } else if(total < 80) {
+//     alert("Дайте два!")
+// } else {
+//     alert("Берём!")
+// }
 
-if (total >= 90) {
-    alert("Не берём!")
-} else if(total < 80) {
-    alert("Дайте два!")
+const red = "нет";
+const yellow = "нет";
+if (red === "да") {
+    alert("При красном сигнале стоим - дорогу переходить нельзя!");
+} else if(yellow === "нет") {
+    alert("При жёлтом сигнале нужно приготовиться, но дорогу пока переходить нельзя!");
 } else {
-    alert("Берём!")
+    alert("Зелёный сигнал - переходим дорогу.");
 }
